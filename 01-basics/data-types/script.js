4c3956df41ca3ffd8e435155f659a0c48912b73e
Pixelraider-sudo <bitcranker@gmail.com>
2026-05-22 19:58:35 +0300

    Learned JavaScript data types

---
+++ 01-basics/data-types/script.js
@@ -0,0 +1,70 @@
+// STRING
+let username = "Pixel";
+
+console.log(username);
+
+// NUMBER
+let age = 19;
+
+console.log(age);
+
+// BOOLEAN
+let isStudent = true;
+
+console.log(isStudent);
+
+// ARRAY
+let favoriteGames = ["eFootball", "Valorant", "FIFA"];
+
+console.log(favoriteGames);
+
+// OBJECT
+let user = {
+  name: "Pixel",
+  country: "Kenya",
+  skill: "JavaScript",
+};
+
+console.log(user);
+
+// NULL
+let emptyValue = null;
+
+console.log(emptyValue);
+
+// UNDEFINED
+let futureGoal;
+
+console.log(futureGoal);
+
+
+// trial
+// Create:
+
+// an array of favorite foods
+// an object about your laptop
+// a boolean called isLearningJS
+// a null variable
+// an undefined variable
+
+let favFood =["banana","rice","chapati"];
+console.log(favFood);
+
+let laptop = {
+    ram:"12gb",
+    videographics:"1200vh",
+};
+console.log(laptop);
+
+let isLearningJs = true;
+console.log(isLearningJs);
+
+let classValue = null;
+console.log(classValue);
+
+let audi;
+console.log(audi);
+
+console.log(typeof username);
+console.log(typeof age);
+console.log(typeof isStudent);
--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Javascript
 
-# JavaScript Variables
+# Variables
 
-Today I learned:
 - let
 - const
 - var
 - console.log()
+
+# Data Types
+
+- strings
+- numbers
+- booleans
+- arrays
+- objects
+- null
+- undefined
+- typeof
