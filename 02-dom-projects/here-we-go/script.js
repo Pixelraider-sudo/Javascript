78a87e08741c90dc83f80eae5cf5253af7021255
Pixelraider-sudo <bitcranker@gmail.com>
2026-06-03 23:50:04 +0300

    small project update

--- 02-dom-projects/here-we-go/script.js
+++ 02-dom-projects/here-we-go/script.js
@@ -1,51 +1,51 @@
-let clock = document.getElementById("clock");
-let greeting = document.getElementById("greeting");
-let quote = document.getElementById("quote");
+// let clock = document.getElementById("clock");
+// let greeting = document.getElementById("greeting");
+// let quote = document.getElementById("quote");
 
-let quoteBtn = document.getElementById("quoteBtn");
-let themeBtn = document.getElementById("themeBtn");
+// let quoteBtn = document.getElementById("quoteBtn");
+// let themeBtn = document.getElementById("themeBtn");
 
-let quotes = [
-  "Keep going and never give up",
-  "You are getting better every day",
-  "Code. Break. Fix. Repeat.",
-  "Discipline beats motivation",
-  "Small steps = big results",
-];
+// let quotes = [
+//   "Keep going and never give up",
+//   "You are getting better every day",
+//   "Code. Break. Fix. Repeat.",
+//   "Discipline beats motivation",
+//   "Small steps = big results",
+// ];
 
-// CLOCK
-function updateClock() {
-  let now = new Date();
+// // CLOCK
+// function updateClock() {
+//   let now = new Date();
 
-  let h = now.getHours();
-  let m = now.getMinutes();
-  let s = now.getSeconds();
+//   let h = now.getHours();
+//   let m = now.getMinutes();
+//   let s = now.getSeconds();
 
-  clock.textContent = `${h}:${m}:${s}`;
+//   clock.textContent = `${h}:${m}:${s}`;
 
-  if (h < 12) greeting.textContent = "Good Morning ";
-  else if (h < 18) greeting.textContent = "Good Afternoon ";
-  else greeting.textContent = "Good Evening ";
-}
+//   if (h < 12) greeting.textContent = "Good Morning ";
+//   else if (h < 18) greeting.textContent = "Good Afternoon ";
+//   else greeting.textContent = "Good Evening ";
+// }
 
-setInterval(updateClock, 1000);
-updateClock();
+// setInterval(updateClock, 1000);
+// updateClock();
 
-// QUOTE BUTTON
-quoteBtn.onclick = function () {
-  let randomIndex = Math.floor(Math.random() * quotes.length);
-  quote.textContent = quotes[randomIndex];
-};
+// // QUOTE BUTTON
+// quoteBtn.onclick = function () {
+//   let randomIndex = Math.floor(Math.random() * quotes.length);
+//   quote.textContent = quotes[randomIndex];
+// };
 
-// THEME TOGGLE
-themeBtn.onclick = function () {
-  document.body.classList.toggle("dark");
+// // THEME TOGGLE
+// themeBtn.onclick = function () {
+//   document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    document.body.style.background = "#111";
-    document.body.style.color = "white";
-  } else {
-    document.body.style.background = "#e17f7f";
-    document.body.style.color = "black";
-  }
-};
+//   if (document.body.classList.contains("dark")) {
+//     document.body.style.background = "#111";
+//     document.body.style.color = "white";
+//   } else {
+//     document.body.style.background = "#e17f7f";
+//     document.body.style.color = "black";
+//   }
+// };
